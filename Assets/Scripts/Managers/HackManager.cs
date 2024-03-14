405dd5aeac1b7ddd1f71fc64ebac70065bd52129
lcscout <lcscoutinho@outlook.com>
2024-03-14 07:50:31 -0300

    Add options to open hack manager based on clicks
    These options avoid opening the hacks panel on accident

--- Assets/Scripts/Managers/HackManager.cs
+++ Assets/Scripts/Managers/HackManager.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HackManager : MonoBehaviour
 {
     #region Fields
-    [Header("References")]
-    [SerializeField] private Button openButton;
+    [Header("Settings")]
+    [SerializeField] private int clickTimesToOpenPanel = 3;
+    [SerializeField] private float maxSecondsToInvalidateClick = .5f;
+    [Header("Debug")]
+    [SerializeField, ReadOnly] private int clickCount;
+    [SerializeField, ReadOnly] private float clickTimer;
+    #endregion
+
+    #region Unity Messages
+    private void Update()
+    {
+        if (clickCount > 0)
+        {
+            clickTimer += Time.deltaTime;
+
+            if (clickTimer > maxSecondsToInvalidateClick)
+            {
+                clickCount = 0;
+                clickTimer = 0;
+            }
+        }
+    }
     #endregion
 
     #region Public Methods
     // method used in unity events of hack manager UI button
-    public void OpenPanel()
+    public void TryOpenPanel()
     {
-        Debug.Log("Test opening hack panel");
+        clickCount++;
+        clickTimer = 0;
+        Debug.Log("Clicked to open panel");
+
+        if (clickCount >= clickTimesToOpenPanel)
+        {
+            clickCount = 0;
+            clickTimer = 0;
+            Debug.Log("Opened hack panel");
+        }
     }
     #endregion
 }
