c57834f03d099ac355f56aea020c454995e743c3
Lucas Coutinho <lucasctnh@hotmail.com>
2024-06-22 09:52:05 -0300

    Add rotation to killed pieces to differentiate

--- Assets/Scripts/Board/KilledPiece.cs
+++ Assets/Scripts/Board/KilledPiece.cs
@@ -1,5 +1,4 @@
 using NaughtyAttributes;
-using System.Drawing;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +7,15 @@ public class KilledPiece : MonoBehaviour
     [Header("Settings")]
 	[SerializeField] private float speed = 16f;
     [SerializeField] private float gravity = 32f;
+    [SerializeField, Range(0f, 1f)] private float transparency = .9f;
 	[Header("Debug")]
 	[SerializeField, ReadOnly] private bool falling;
 	[SerializeField, ReadOnly] private Vector2 moveDir;
 	[SerializeField, ReadOnly] private RectTransform rect;
 	[SerializeField, ReadOnly] private Image img;
+	[SerializeField, ReadOnly] private float randomRot;
 
-    public bool Falling => falling;
+	public bool Falling => falling;
 
     public void Initialize(Sprite pice, Vector2 start, float size)
     {
@@ -23,13 +24,17 @@ public class KilledPiece : MonoBehaviour
         moveDir = Vector2.up;
         moveDir.x = Random.Range(-1.0f, 1.0f);
         moveDir *= speed / 2;
+        randomRot = Random.Range(.8f, 1.3f);
 
-        img = GetComponent<Image>();
+		img = GetComponent<Image>();
         img.raycastTarget = false;
         rect = GetComponent<RectTransform>();
         img.sprite = pice;
         rect.anchoredPosition = start;
 		rect.sizeDelta = new Vector2(size, size);
+        Color newC = img.color;
+        newC.a = transparency;
+        img.color = newC;
 	}
 
 	// Update is called once per frame
@@ -43,10 +48,11 @@ public class KilledPiece : MonoBehaviour
         moveDir.y -= Time.deltaTime * gravity;
         moveDir.x = Mathf.Lerp(moveDir.x, 0, Time.deltaTime);
         rect.anchoredPosition += moveDir * Time.deltaTime * speed;
+        rect.Rotate(new Vector3(0, 0, randomRot));
 
         if (rect.position.x < -Match3.PieceSize || rect.position.x > Screen.width + Match3.PieceSize || rect.position.y < -Match3.PieceSize|| rect.position.y > Screen.height + Match3.PieceSize)
         {
             falling = false;
-        }
+		}
     }
 }
